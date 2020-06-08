44bad7089828332cbe10319898b0ad746edf3ff8
akashgkrishnan <krishnanag1996@gmail.com>
2020-06-08 22:24:49 +0530

    calculator.html

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', function (request, response){
-  response.send('inside calcss');
-})
+app.use(bodyParser.urlencoded({extended: true}));
 
 
-app.listen(3000, function(){
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(request, response) {
+  console.log(request.body);
+  sum = Number(request.body.firstNumber) + Number(request.body.secondNumber);
+  response.send('sum is '+ sum);
+});
+
+app.get('/bmi', function(request, response){
+  response.sendFile(__dirname + '/bmicalculator.html');
+});
+
+app.post("/bmi", function(request, response){
+  weight = parseFloat(request.body.Weight);
+  height = parseFloat(request.body.height);
+  var bmi = weight/ (height * height);
+  response.send('your BMI is ' + bmi);
+});
+
+
+
+app.listen(3000, function() {
   console.log('started server for calculator');
-})
+});
