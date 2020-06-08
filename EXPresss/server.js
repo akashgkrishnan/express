a247f63a517051ba39d283270636e6d55b747c5b
akashgkrishnan <krishnanag1996@gmail.com>
2020-06-08 10:25:08 +0530

    done

---
+++ EXPresss/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express()
+
+app.get('/', function (request, response){
+  response.send('<h1>hello! World </h1>');
+})
+
+app.get('/contact', function (request, response){
+  response.send('contact');
+})
+
+app.get('/about', function (request, response){
+  response.send('about');
+})
+
+app.get('/hello', function (request, response){
+  response.send('ello');
+})
+
+
+app.get('/calculator', function(request, response){
+  response.send('hi world')
+})
+
+
+
+app.listen(3000, function (){
+  console.log('port started at 3000');
+});
